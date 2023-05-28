b1df1589f2b217eea039f40188a471631e3e656e
raagdol <ju8559@naver.com>
2023-05-28 10:02:29 +0900

    회원가입 기능 완료

--- src/component/MemJoin.jsx
+++ src/component/MemJoin.jsx
@@ -4,7 +4,6 @@ import styles from '../component/MemJoin.module.css';
 import join from '../img/join.png';
 
 function MemJoin() {
-
   const [member, setMember] = useState({
     userName: '',
     phoneNum: '',
@@ -26,13 +25,16 @@ function MemJoin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // 모든 입력값이 정확히 입력되었는지 확인
     for (let key in member) {
       if (!member[key]) {
         alert(`${key}를 입력해주세요.`);
         return;
       }
     }
-    
+
+    // 유효성 검사 통과 여부 확인
     if (isPhoneNumValid && isRrNumValid) {
       joinClick();
     } else {
@@ -44,7 +46,7 @@ function MemJoin() {
       }
     }
   };
-  
+
   const joinClick = async () => {
     try {
       const formattedMember = {
@@ -68,82 +70,82 @@ function MemJoin() {
 
   return (
     <>
-    <div className={styles.bg}>
-      <div className={styles.join}>
-      <img src={join} alt="회원가입"></img>
-    </div>
+      <div className={styles.bg}>
+        <div className={styles.join}>
+          <img src={join} alt="회원가입"></img>
+        </div>
 
-    <form onSubmit={handleSubmit}>
-      <h4 htmlFor="username" className={styles.name}>이름</h4>
-      <input 
-        type="name"
-        id="username"
-        className={styles.username}
-        value={member.userName}
-        required
-        placeholder="이름"
-        onChange={(e) => {
-          setMember({...member, userName: e.target.value});
-        }}  
-      />
+        <form onSubmit={handleSubmit}>
+          <h4 htmlFor="username" className={styles.name}>이름</h4>
+          <input 
+            type="name"
+            id="username"
+            className={styles.username}
+            value={member.userName}
+            required
+            placeholder="이름"
+            onChange={(e) => {
+              setMember({...member, userName: e.target.value});
+            }}  
+          />
 
-      <h4 htmlFor="phonenum" className={styles.id}>전화번호</h4>
-      <input 
-        type="tel"
-        id="phonenum"
-        className={styles.phonenum}
-        value={member.phoneNum}
-        required
-        placeholder='010-0000-0000'
-        onChange={(e) => {
-          setMember({...member, phoneNum: e.target.value});
-        }}
-      />
+          <h4 htmlFor="phonenum" className={styles.id}>전화번호</h4>
+          <input 
+            type="tel"
+            id="phonenum"
+            className={styles.phonenum}
+            value={member.phoneNum}
+            required
+            placeholder='010-0000-0000'
+            onChange={(e) => {
+              setMember({...member, phoneNum: e.target.value});
+            }}
+          />
 
-      <h4 htmlFor="phonenum" className={styles.id}>전화번호 확인</h4>
-      <input
-        type='tel'
-        id='phonenum'
-        className={styles.phonenum}
-        required
-        placeholder='동일한 전화번호를 입력해주세요.'
-        onChange={(e) => {
-          setMember({...member, phoneNumToCheck: e.target.value})
-          setIsPhoneNumValid(false);
-        }}
-        onBlur={checkPhoneNum}
-      />
+          <h4 htmlFor="phonenum" className={styles.id}>전화번호 확인</h4>
+          <input
+            type='tel'
+            id='phonenum'
+            className={styles.phonenum}
+            required
+            placeholder='동일한 전화번호를 입력해주세요.'
+            onChange={(e) => {
+              setMember({...member, phoneNumToCheck: e.target.value});
+              setIsPhoneNumValid(false);
+            }}
+            onBlur={checkPhoneNum}
+          />
 
-      <h4 htmlFor="rrnum" className={styles.pw}>주민등록번호</h4>
-      <input 
-        type="tel"
-        id="rrnum"
-        className={styles.rrnum}
-        value={member.rrNum}
-        required
-        placeholder='000000-0000000'
-        onChange={(e) => {
-          setMember({...member, rrNum: e.target.value});
-        }}
-      />
+          <h4 htmlFor="rrnum" className={styles.pw}>주민등록번호</h4>
+          <input 
+            type="tel"
+            id="rrnum"
+            className={styles.rrnum}
+            value={member.rrNum}
+            required
+            placeholder='000000-0000000'
+            onChange={(e) => {
+              setMember({...member, rrNum: e.target.value});
+            }}
+          />
 
-      <h4 htmlFor="rrnum" className={styles.pw}>주민등록번호 확인</h4>
-      <input 
-        type="tel"
-        id="rrnum"
-        className={styles.rrnum}
-        required
-        placeholder='동일한 주민등록번호를 입력해주세요.'
-        onChange={(e) => {
-          setMember({...member, rrNumToCheck: e.target.value});
-          setIsRrNumValid(false);
-        }}
-        onBlur={checkRrNum}
-      />
-    </form>
-      
-    <button type="submit" className={styles.joinbtn} onClick={joinClick}>JOIN</button>
-  </div>
+          <h4 htmlFor="rrnum" className={styles.pw}>주민등록번호 확인</h4>
+          <input 
+            type="tel"
+            id="rrnum"
+            className={styles.rrnum}
+            required
+            placeholder='동일한 주민등록번호를 입력해주세요.'
+            onChange={(e) => {
+              setMember({...member, rrNumToCheck: e.target.value});
+              setIsRrNumValid(false);
+            }}
+            onBlur={checkRrNum}
+          />
+        
+          <button type="submit" className={styles.joinbtn}>JOIN</button>
+        </form>
+      </div>
     </>
   );
 }
