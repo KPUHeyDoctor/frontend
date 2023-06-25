ef9d963ce6faebe46184d106d2c750f9de0381ca
raagdol <ju8559@naver.com>
2023-06-25 21:40:41 +0900

    fix: 예약 의사 정보

--- src/component/Reserdoc.jsx
+++ src/component/Reserdoc.jsx
@@ -3,49 +3,46 @@ import styles from '../component/Reserdoc.module.css';
 import doc01 from '../img/doc01.png';
 import doc02 from '../img/doc02.png';
 
-function Reserdoc() {
+function Reserdoc({ doctors }) {
   const [selectedDoctor, setSelectedDoctor] = useState(null);
-  const [doctorInfo, setDoctorInfo] = useState(null);
+  const [enterpriseName, setEnterpriseName] = useState('');
 
   useEffect(() => {
-    if (selectedDoctor) {
-      fetchDoctorInfo(selectedDoctor);
+    if (doctors && doctors.length > 0) {
+      setSelectedDoctor(doctors[0]);
+      setEnterpriseName(doctors[0].BIZPLC_NM);
     }
-  }, [selectedDoctor]);
-
-  const fetchDoctorInfo = async (doctorName) => {
-    try {
-      const response = await fetch(`/api/reservation/doctor?doctorName=${doctorName}`);
-      const data = await response.json();
-      setDoctorInfo(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleDoctorSelect = (doctorName) => {
-    setSelectedDoctor(doctorName);
-  };
+  }, [doctors]);
 
   return (
     <div className={styles.bg}>
-      <h2 className={styles.hosname}>병원</h2>
-      <div className={styles.doctor}>
-        <button className={styles.doc01} onClick={() => handleDoctorSelect('장인')}>
-          <img src={doc01} alt="의사1" />
-        </button>
-        <button className={styles.doc02} onClick={() => handleDoctorSelect('장인2')}>
-          <img src={doc02} alt="의사2" />
-        </button>
-      </div>
-      {doctorInfo && (
-        <div className={styles.doctorInfo}>
-          <h3>{doctorInfo[0].doctorName}</h3>
-          <p>진료과: {doctorInfo[0].doctorMedical}</p>
-          <p>전문 분야: {doctorInfo[0].doctorField}</p>
-          <p>진료 시간: {doctorInfo[0].doctorTime}</p>
+      <h2 className={styles.hosname}>{enterpriseName}</h2>
+      <div className={styles.container}>
+        <div className={styles.doctor}>
+          <button className={styles.doc01}>
+            <img src={doc01} alt="의사1" />
+            {selectedDoctor && (
+              <div className={styles.doctorInfo}>
+                <h3>{selectedDoctor.doctorName}</h3>
+                <p>진료과: {selectedDoctor.doctorMedical}</p>
+                <p>학력: {selectedDoctor.doctorField}</p>
+                <p>휴진: {selectedDoctor.doctorTime}</p>
+              </div>
+            )}
+          </button>
+          <button className={styles.doc02}>
+            <img src={doc02} alt="의사2" />
+            {selectedDoctor && (
+              <div className={styles.doctorInfo}>
+                <h3>{selectedDoctor.doctorName}</h3>
+                <p>진료과: {selectedDoctor.doctorMedical}</p>
+                <p>학력: {selectedDoctor.doctorField}</p>
+                <p>휴진: {selectedDoctor.doctorTime}</p>                
+              </div>
+            )}
+          </button>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- src/component/Resertime.jsx
+++ src/component/Resertime.jsx
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import styles from '../component/Resertime.module.css';
-import TimePicker from 'react-time-picker';
 
 function Resertime() {
-  const [time, setTime] = useState('10:00');
-
-  const handleTimeChange = (time) => {
-    setTime(time);
-  };
 
   return(
     <div className={styles.bg}>
       <h2 className={styles.hosname}>병원</h2>
-      <TimePicker
-        onChange = {handleTimeChange}
-        value={time}
-      />
       <span className={styles.pass}>
         <Link to ="/reser">
           <button className={styles.before}>이전</button>
