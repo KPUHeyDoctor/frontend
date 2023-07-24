5e025322eb572ecfb487cbf83ad36dfcc611a7b6
raagdol <ju8559@naver.com>
2023-07-24 15:42:10 +0900

    fix: 예약 클릭시 의사 컴포넌트로 이동

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import MemberLogin from './page/MemberLogin.jsx';
 import EnterLogin from './page/EnterLogin.jsx';
 import MemberJoin from './page/MemberJoin.jsx';
 import EnterJoin from './page/EnterJoin.jsx';
+
+import Resdoc from './component/Reserdoc.jsx'
+
 import ReserDoc from './page/ReserDoc.jsx';
 import ReserTime from './page/ReserTime.jsx';
 import MyPage from './page/MyPage.jsx';
@@ -21,11 +24,15 @@ import ChatEbinPage from './page/ChatEbinPage.jsx';
 import ChatKidsPage from './page/ChatKidsPage.jsx';
 import ChatBonePage from './page/ChatBonePage.jsx';
 
+
 function App() {
 
   return(
     <BrowserRouter>
       <Routes>
+
+        <Route path="/reserdoc" element={<Resdoc />}/>
+
         <Route path="/" element={<MainPage />}/>
         <Route path="/EntMainPage" element={<EntMainPage/>}/>
         <Route path="/choologin" element={<ChooseLogin />}/>
--- src/component/KakaoMap.jsx
+++ src/component/KakaoMap.jsx
@@ -208,7 +208,7 @@ function KakaoMap() {
         .get(`https://tukdoctor.shop/api/reservation?enterpriseName=${enterpriseName}`)
         .then((response) => {
           const hospitalInfo = response.data;
-          navigate('/reser', { hospitalInfo });
+          navigate('/reserdoc', { hospitalInfo });
         })
         .catch((error) => {
           console.log(error);
