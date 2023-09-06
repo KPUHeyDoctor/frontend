d4a7076fb9afc2660d297de67a6a4351ddde499e
raagdol <ju8559@naver.com>
2023-09-07 00:48:22 +0900

    feat: 마이페이지 예약내역 등록

--- src/component/Mypage.jsx
+++ src/component/Mypage.jsx
@@ -1,25 +1,51 @@
-
+import React, { useState, useEffect } from 'react';
 import styles from '../component/Mypage.module.css';
 import mypage from '../img/mypage.png';
+import axios from 'axios';
 
-function Mypage({ reservations }) {
-  
+function Mypage() {
+  const [reservations, setReservations] = useState([]);
+  const [reservationMessage, setReservationMessage] = useState('');
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const storedUserName = localStorage.getItem('userName');
+    if (storedUserName) {
+      setUserName(storedUserName);
+
+      axios.get(`api/history/user?userName=${storedUserName}`)
+        .then((response) => {
+          const sortedReservations = response.data.reverse();
+          setReservations(sortedReservations);
+        })
+        .catch((error) => {
+          console.error('API 요청 에러:', error);
+        });
+    }
+  }, []);
+
   return (
     <>
       <div className={styles.bg}>
         <div className={styles.mypage}>
           <img src={mypage} alt="마이페이지" />
         </div>
-        <div className={styles.list}>
-          <ul>
-            {reservations.map((reservation, index) => (
-              <li key={index}>{reservation}</li>
-            ))}
-          </ul>
-        </div>
+        {userName && (
+          <>
+            <h2 className={styles.userName}>{userName}님의 예약 내역</h2>
+            <div className={styles.list}>
+              <ul>
+                {reservations.map((reservation, index) => (
+                  <li key={index}>{reservation.doctorname} - {reservation.historyTime} - {reservation.historyBoolean ? '확인됨' : '미확인'}</li>
+                ))}
+              </ul>
+            </div>
+            {reservationMessage && <p className={styles.reservationMessage}>{reservationMessage}</p>}
+          </>
+        )}
       </div>
     </>
   );
-  }
+}
 
 export default Mypage;
